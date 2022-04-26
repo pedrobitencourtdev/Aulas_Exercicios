acc71612176664e06d61783dcd5abfbc0d56d5cf
ZeuS <104322778+zeusprogramador@users.noreply.github.com>
2022-04-26 18:42:33 -0300

    Att 2.0
    Inclui algumas funções

--- ex037.py
+++ ex037.py
@@ -1,3 +1,7 @@
-#comando Upper para deixar maiúsculo
-
-frase = 'Chei de marra novin!'
+#comando Upper para deixar maiúsculo que é um método
+#aqui o que for maiúsculo ele mantém o que for minúsculo ele joga pra maiúsculo
+#se eu usar frase.lower() fica tudo minúsculo
+frase = 'Olá Mundo eu sou o Biridin programador'
+#var = frase.upper()
+print(frase.upper())
+print(frase.replace('Olá', 'Oi'))
---
+++ ex038.py
@@ -0,0 +1,7 @@
+#função .capitalize() e função title
+
+frase = 'Olá meu novo nome é biridin'
+print(frase.capitalize())
+#a função title joga todas as palavras da frase com iniciais maiúsculas.
+frase = 'Olá meu novo nome é biridin'
+print(frase.title())
