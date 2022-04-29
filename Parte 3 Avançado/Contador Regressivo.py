2afc3100edbe66ec3feb4c66789c9e3846ab1610
ZeuS <www.pedrobitencourt@hotmail.com>
2022-04-29 03:16:25 -0300

    Att

---
+++ Parte 3 Avançado/Contador Regressivo.py
@@ -0,0 +1,14 @@
+#contador regressivo para fogos de artifícios for in range laços de repetições
+from time import sleep
+from emoji import emojize
+x = 'Você está preparado?'
+print('-=' * 10)
+print('{:=^10}'.format(x))
+print('-=' * 10)
+cont = 11
+for c in range(cont, 0, -1):
+    cont += -1
+    sleep(1)
+    print(cont)
+
+print('BoOoooOoM! Feliz Ano Novo! {}'.format(emojize(':sparkles:')))
---
+++ Parte 3 Avançado/Impar e par de 1 a 50.py
@@ -0,0 +1,4 @@
+#pegar os números pares que estão entre 1 e 50 e printar na tela
+print('Aqui está todos os números pares entre 1 e 50')
+for c in range(2, 51, 2):
+    print(c)
