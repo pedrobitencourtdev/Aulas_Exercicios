f0c856cd41ef32086cda825066f1ec9755888dbd
ZeuS <www.pedrobitencourt@hotmail.com>
2022-05-02 03:30:16 -0300

    Update JoKenPo.py

--- Projetos/JoKenPo.py
+++ Projetos/JoKenPo.py
@@ -20,7 +20,7 @@ while contador <= 10:
             sair = True
             print('Você saiu do jogo!')
             break
-        elif computador == 1:   #computador jogou PEDRA#
+        elif computador == 1:  # computador jogou PEDRA#
             if jogador == 1:
                 print('\033[1;32mO você escolheu: {}\033[m'.format(itens[jogador]))
                 print('JO!')
@@ -70,7 +70,7 @@ while contador <= 10:
                 print('O placar é JOGADOR {} VS {} COMPUTADOR'.format(placarJ, placarC))
             else:
                 print('\033[1;31mJogada Inválida.\033[1;33m')
-        elif computador == 2:  #computador jogou PAPEL
+        elif computador == 2:  # computador jogou PAPEL
             if jogador == 1:
                 print('\033[1;32mO você escolheu: {}\033[m'.format(itens[jogador]))
                 placarC += 1
@@ -119,8 +119,8 @@ while contador <= 10:
                 print('-=' * 10)
                 print('O placar é JOGADOR {} VS {} COMPUTADOR'.format(placarJ, placarC))
             else:
-               print('\033[1;31mJogada Inválida.\033[1;33m')
-        elif computador == 3: #computador jogou TESOURA
+                print('\033[1;31mJogada Inválida.\033[1;33m')
+        elif computador == 3:  # computador jogou TESOURA
             if jogador == 1:
                 print('\033[1;32mO você escolheu: {}\033[m'.format(itens[jogador]))
                 placarJ += 1
@@ -173,4 +173,3 @@ while contador <= 10:
 
     except ValueError:
         print('\033[1;31mvocê só pode digitar números de 0 até 3!\033[m')
-
